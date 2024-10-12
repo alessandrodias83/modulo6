92287df668dd1ce15e41dfee3332f7e21ce8159d
Alessandro <158523798+alessandrodias83@users.noreply.github.com>
2024-10-12 13:56:05 -0300

    att

---
+++ modulo06/receitas/js/scripts.js
@@ -0,0 +1,5 @@
+const receitas = [
+    { titulo: "Lasanha", ingredientes: "Massa, molho, recheio", preparo: "Coloque camadas em uma forma e asse por 30 minutos"},
+    { titulo: "Strogonoff", ingredientes: "Carne, creme de leite, mostarda", preparo: "Refogue tudo em uma panela"},
+    { titulo: "Pão", ingredientes: "Farinha, ovos, fermento", preparo: "Misture tudo, deixe descansar e asse"}
+]
